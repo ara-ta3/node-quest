592765ef5de02c38baa7e829981e44eb5d63da45
Arata Tanaka <tarata43@yahoo.co.jp>
2016-03-05 14:51:36 +0900

    Add parameter and remove toString

--- src/game/model/user.js
+++ src/game/model/user.js
@@ -2,22 +2,27 @@ const Status = require(__dirname + "/status.js");
 const Point  = require(__dirname + "/Point.js");
 
 class User {
-    constructor(id, name, status, equipment) {
+    constructor(id, name, status, equipment, parameter) {
         this.id = id;
         this.name = name;
         this.status = status;
         this.equipment = equipment;
+        this.parameter = parameter;
     };
 
-    attack(target) {
-        let damage = new Point(
-                this.equipment.weapon.averageOfAttack,
+    attack(target, damage) {
+        damage = new Point(
+                damage + this.equipment.weapon.averageOfAttack,
                 this.equipment.weapon.divergenceOfAttack
                 ).toInt();
         return target.damaged(damage);
     };
 
     cure(target, point) {
+        point = new Point(
+                point + this.parameter.mindPower,
+                this.parameter.mindStability
+                ).toInt();
         return target.cured(point);
     };
 
@@ -42,11 +47,6 @@ class User {
     isDead() {
         return this.status.isDead();
     };
-
-    toString() {
-        return `Name: ${this.name}, Status: ${this.status.toString()}`;
-    };
-
 }
 
 module.exports = User;
