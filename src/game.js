b224531a9d93eb399ae042b92080aef05a60be7b
Arata Tanaka <tarata43@yahoo.co.jp>
2016-03-05 12:18:07 +0900

    Add tests and bit refactor

--- src/game.js
+++ src/game.js
@@ -3,17 +3,22 @@ let Status = require(`${__dirname}/game/model/status.js`);
 
 class Game {
     // TODO management of attack point
-    constructor(users, maxHp) {
-        this.users = users;
+    constructor(minHp, maxHp) {
+        this.users = [];
+        this.minHp = minHp;
         this.maxHp = maxHp;
     };
+
+    setUsers(users) {
+        this.users = users;
+    }
 
     findUser(name) {
         let targets = this.users.filter((u) => u.name === name);
         return targets.length === 0 ? null : targets.pop();
     };
 
-    static defaultStatus() {
+    defaultStatus() {
         return new Status(this.maxHp, this.maxHp);
     };
 }
