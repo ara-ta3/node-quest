dfda8274a04a14428c2b2b6014cecddb33efaaef
Arata Tanaka <tarata43@yahoo.co.jp>
2016-04-12 00:52:59 +0900

    Add hit rate

--- test/game/model/User.js
+++ test/game/model/User.js
@@ -5,12 +5,13 @@ const Game = require(`${__dirname}/../../../src/game.js`).Game;
 const Equipment = require(`${__dirname}/../../../src/game/model/Equipment.js`);
 const Weapon = require(`${__dirname}/../../../src/game/model/Weapon.js`);
 const Parameter = require(`${__dirname}/../../../src/game/model/Parameter.js`);
+const HitRate   = require(`${__dirname}/../../../src/game/model/HitRate.js`);
 
 describe("User", () => {
     describe("attack", () => {
         it("should decrease target HP", () => {
             let game    = new Game(0, 10);
-            let equipment = new Equipment(new Weapon(0, 0));
+            let equipment = new Equipment(new Weapon(0, 0, new HitRate(100)));
             let actor   = new User("id1", "A", new Status(game, 10), equipment);
             let target  = new User("id2", "B", new Status(game, 10), equipment);
             actor.attack(target, 5);
@@ -19,7 +20,7 @@ describe("User", () => {
 
         it("should increase attack point with weapon", () => {
             let game    = new Game(0, 10);
-            let equipment = new Equipment(new Weapon(5, 0));
+            let equipment = new Equipment(new Weapon(5, 0, new HitRate(100)));
             let actor   = new User("id1", "A", new Status(game, 10), equipment);
             let target  = new User("id2", "B", new Status(game, 10), equipment);
             actor.attack(target, 5);
@@ -28,7 +29,7 @@ describe("User", () => {
     });
 
     describe("cure", () => {
-        let equipment = new Equipment(new Weapon(0, 0));
+        let equipment = new Equipment(new Weapon(0, 0, new HitRate(100)));
         let dummyParameter = new Parameter(0, 0);
         it("should increase target HP", () => {
             let game    = new Game(0, 10);
