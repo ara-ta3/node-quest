3cedbee1e64bee62e3386f4ece7c8e8efebae343
Arata Tanaka <tarata43@yahoo.co.jp>
2016-07-14 20:05:43 +0900

    Add critical hit

--- src/game/model/Weapon.js
+++ src/game/model/Weapon.js
@@ -1,12 +1,36 @@
+const Point         = require(__dirname + "/Point.js");
+
 class Weapon {
-    constructor(averageOfAttack, divergenceOfAttack, hitRate) {
+    constructor(averageOfAttack, divergenceOfAttack, hitRate, critical) {
         this.averageOfAttack = averageOfAttack;
         this.divergenceOfAttack = divergenceOfAttack;
         this.hitRate = hitRate;
+        this.critical = critical;
+    }
+
+    critical() {
+        return this.critical.hit();
     }
 
     hit() {
         return this.hitRate.hit();
+    }
+
+    damageTo(target) {
+        const hit = this.hit();
+        const critical = hit ? this.critical() : false;
+        const point = hit ? Point.fromWeapon(this).toInt() * (critical ? 2 : 1) : 0;
+        hit && target.damaged(point);
+
+        return Weapon.attackResult(point, hit, critical);
+    }
+
+    static attackResult(damage, hit, critical) {
+        return {
+            value: damage,
+            hit: hit,
+            critical: critical
+        };
     }
 }
 
